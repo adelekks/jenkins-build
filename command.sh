0c841f06a4f8d5fa5afa8ad748ac10a0dce61850
Kenny Adeleke <adelekks@gamil.com>
2021-10-26 19:20:05 -0500

    chmod on command.sh

--- command.sh
+++ command.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# if condition is true
+# if condition is Maven
 if [ "maven" == "$1" ];
 then
     echo "Maven equals $1"
     mvn --version
 fi
 
-# if condition is false
+# if condition is Nodejs
 if [ "nodejs" == "$1" ];
 then
     echo "Nodejs equals $1"
     npm -v
+fi
+
+# if condition is Any
+if [ "any" == "$1" ];
+then
+    echo "Please pick from one of the parameter app"
+    exit 1
 fi
