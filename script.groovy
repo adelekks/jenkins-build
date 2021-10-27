db13bcda30e8257b75330c64365172560d10b4a9
Kenny Adeleke <adelekks@gamil.com>
2021-10-26 22:51:56 -0500

    chmod on command.sh

--- script.groovy
+++ script.groovy
@@ -1,11 +1,9 @@
 def MavenApp() {
     echo "Maven equals ${agent_label}"
-    /usr/bin/mvn -v
 }
 
 def NodeJSApp() {
     echo "Nodejs equals ${agent_label}"
-    /usr/bin/npm -v
 }       
 
 return this
